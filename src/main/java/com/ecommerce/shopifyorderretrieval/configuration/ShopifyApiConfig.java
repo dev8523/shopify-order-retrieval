a22566ddd42d0a78fa5590b256548a6f0035fce6
Debasish Sahoo <debasishsahoo8523@gmail.com>
2023-11-27 00:49:58 +0530

    Use lombok

--- src/main/java/com/ecommerce/shopifyorderretrieval/configuration/ShopifyApiConfig.java
+++ src/main/java/com/ecommerce/shopifyorderretrieval/configuration/ShopifyApiConfig.java
@@ -1,8 +1,12 @@
 package com.ecommerce.shopifyorderretrieval.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "shopify.api")
 public class ShopifyApiConfig {
@@ -10,38 +14,4 @@ public class ShopifyApiConfig {
     private String apiKey;
     private String apiSecret;
     private String accessToken;
-
-    // Getters and setters
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getApiKey() {
-        return apiKey;
-    }
-
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-
-    public String getApiSecret() {
-        return apiSecret;
-    }
-
-    public void setApiSecret(String apiSecret) {
-        this.apiSecret = apiSecret;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
 }
